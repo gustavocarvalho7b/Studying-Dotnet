5be85e8ba39e67b09a11100b48ea0c7983fca364
Gustavo Britto <gustavocarvalho7b@gmail.com>
2026-07-31 17:04:08 -0300

    Primeiro commit

---
+++ backend/MinhaApi/Program.cs
@@ -0,0 +1,34 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"Entrou: {context.Request.Path}");
+
+    await next();
+
+    Console.WriteLine($"Saiu: {context.Response.StatusCode}");
+});
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
